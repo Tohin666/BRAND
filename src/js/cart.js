7901d4eb04037a1bf65ecdd655e46772557f01f7
Tohin666 <tohin666@gmail.com>
2018-07-08 00:10:00 +0300

    making cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,54 +1,141 @@
 (function ($) {
 
-function buildGoodsList() {
-  $.get('http://localhost:5000/goods', {}, function (goods) {
-    $('#goods').empty();
-    goods.forEach(function (good) {
-      var $button = $('<button/>', {
-        text: 'Buy',
-        class: 'buy',
-        'data-id': good.id,
-        'data-price': good.price,
-        'data-name': good.name,
-        'data-quantity': good.quantity
+  function buildGoodsList() {
+    $.get('http://localhost:5000/goods', {}, function (goods) {
+      $('#goods').empty();
+      goods.forEach(function (good) {
+        var $button = $('<button/>', {
+          text: 'Buy',
+          class: 'buy',
+          'data-id': good.id,
+          'data-price': good.price,
+          'data-name': good.name,
+          'data-src': good.src
+        });
+        $('<li/>', {
+          text: good.name + ' (' + good.quantity + ')'
+        }).append($button).appendTo('#goods');
       });
-      $('<li/>', {
-        text: good.name + ' (' + good.quantity + ')'
-      }).append($button).appendTo('#goods');
-    });
-  }, 'json');
-}
+    }, 'json');
+  }
 
-function buildCart() {
-  $.get('http://localhost:5000/cart', {}, function (items) {
-    $('#cart').empty();
-    var $ul = $('<ul/>');
-    var total = 0;
-    items.forEach(function (item) {
-      total += +item.price * +item.quantity;
-      var $li = $('<li/>', {
-        text: item.name + ': ' + item.price + ' rub.',
-        'data-id': item.id,
-        'data-quantity': item.quantity
+  function buildCart() {
+    $.get('http://localhost:5000/cart', {}, function (items) {
+      $('#cart').empty();
+      var $ul = $('<ul/>');
+      var total = 0;
+
+      items.forEach(function (item) {
+        total += +item.price * +item.quantity;
+        var $li = $('<li/>', {
+          text: item.name + ': ' + item.price + ' rub.',
+          'data-id': item.id,
+          'data-quantity': item.quantity
+        });
+        $li.append($('<button/>', {
+          text: 'X',
+          class: 'removeButton'
+        }));
+        $ul.append($li);
+
+        var $divCartListItem = $('<div/>').addClass('cart-list__item');
+        var $divCartListColumns = $('<div/>').addClass('cart-list__columns');
+        var $divCartListProductDetails = $('<div/>').addClass('cart-list__Product-Details');
+        var $aImgCartlistImg = $('<a/>')
+          .attr('href', 'single-page.html')
+          .append($('<img/>', {
+            class: "cart-list__img",
+            src: item.src,
+            alt: "cart-item1"
+          }));
+
+        var $divCartListDesc = $('<div/>').addClass('cart-list__desc');
+        $divCartListDesc.append($('<div/>').addClass('cart-list__name')
+          .append($('<a/>', {
+            href: "single-page.html",
+            text: item.name
+          })));
+        $divCartListDesc.append($('<div/>').text('Color: ')
+          .append($('<span/>', {
+            class: "cart-list__color-size",
+            text: 'Red'
+          })));
+        $divCartListDesc.append($('<div/>').text('Size: ')
+          .append($('<span/>', {
+            class: "cart-list__color-size",
+            text: 'Xll'
+          })));
+
+        var $divCartListUnitePrice = $('<div/>').addClass('cart-list__unite-Price').text('$' + item.price);
+        var $divCartListQuantity = $('<div/>').addClass('cart-list__Quantity')
+          .append($('<input>', {
+            type: "text",
+            placeholder: item.quantity,
+            pattern: "[0–9]{1,5}",
+            "data-id": item.id,
+            class: "inputQuantityCart",
+            "data-name": item.name,
+            "data-price": item.price,
+            "data-src": item.src
+          }));
+        var $divCartListShipping = $('<div/>').addClass('cart-list__shipping').text('FREE');
+        var $divCartListSubtotal = $('<div/>').addClass('cart-list__Subtotal').text('$300');
+        var $divCartListACTION = $('<div/>').addClass('cart-list__ACTION')
+          .append($('<a/>')
+            .attr('href', '#')
+            .append($('<i/>', {
+              class: "fas fa-times-circle"
+            })));
+
+        $divCartListProductDetails.append($aImgCartlistImg);
+        $divCartListProductDetails.append($divCartListDesc);
+        $divCartListColumns.append($divCartListProductDetails);
+        $divCartListColumns.append($divCartListUnitePrice);
+        $divCartListColumns.append($divCartListQuantity);
+        $divCartListColumns.append($divCartListShipping);
+        $divCartListColumns.append($divCartListSubtotal);
+        $divCartListColumns.append($divCartListACTION);
+        $divCartListItem.append($divCartListColumns);
+        $('.cart-list').append($divCartListItem);
       });
-      $li.append($('<button/>', {
-        text: 'X',
-        class: 'removeButton'
-      }));
-      $ul.append($li);
-    });
-    $('#cart').append($ul);
-    $('#cart').append('Total: ' + total + ' rub.' + '<br>');
 
-    var $clearButton = $('<button/>', {
-      text: 'Cleat Cart',
-      class: 'clearButton',
-      id: 'clearButton'
-    });
-    $('#cart').append($clearButton)
-  }, 'json');
-}
+      //Handler manual change good quantity.
+      $('.cart-list__item').on('change', 'input', function (event) {
+        alert('!!!!!!!!!');
+        var cartGood = $('.cart-list__Quantity input[data-id="' + $(this).attr('data-id') + '"]');
 
+        var good = {
+          id: $(this).attr('data-id'),
+          quantity: +cartGood.eq(0).attr('placeholder') + 1,
+          name: $(this).attr('data-name'),
+          price: $(this).attr('data-price'),
+          src: $(this).attr('data-src')
+        };
+
+        $.ajax({
+          url: 'http://localhost:5000/cart/' + good.id,
+          type: 'PUT',
+          data: good,
+          success: function () {
+            buildCart();
+            buildGoodsList();
+          }
+        });
+
+        event.preventDefault();
+      });
+
+      $('#cart').append($ul);
+      $('#cart').append('Total: ' + total + ' rub.' + '<br>');
+
+      var $clearButton = $('<button/>', {
+        text: 'Cleat Cart',
+        class: 'clearButton',
+        id: 'clearButton'
+      });
+      $('#cart').append($clearButton)
+    }, 'json');
+  }
 
 
   $(function () {
@@ -56,14 +143,12 @@ function buildCart() {
     buildGoodsList();
 
     $('#goods').on('click', '.buy', function (event) {
-      if (+$(this).attr('data-quantity') < 1) {
-        alert('Недостаточно товара');
-        return;
-      }
+
       var good = {
         id: $(this).attr('data-id'),
         name: $(this).attr('data-name'),
-        price: $(this).attr('data-price')
+        price: $(this).attr('data-price'),
+        src: $(this).attr('data-src')
       };
 
       var cartGood = $('#cart li[data-id="' + $(this).attr('data-id') + '"]');
@@ -88,6 +173,7 @@ function buildCart() {
       }
       event.preventDefault();
     });
+
 
     $('#cart').on('click', '#clearButton', function (event) {
       $.get('http://localhost:5000/cart/', {}, function (items) {
