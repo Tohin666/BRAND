efaac7fdcd8ec667ef5b8f796e628de171cf49fa
Tohin666 <tohin666@gmail.com>
2018-07-15 12:22:10 +0300

    search complete

---
+++ src/js/searchForItem.js
@@ -0,0 +1,48 @@
+jQuery(document).ready(function($){
+  
+  $.get('http://localhost:5000/goods/', {}, function (items) {
+    items.forEach(function (item) {
+      var $aItem = $('<a/>', {
+        text: item.name,
+        href: 'single-page.html'
+      });
+      var $liItem = $('<li/>').append($aItem);
+      // console.log($liItem)
+      $('.live-search-list').append($liItem)
+    });
+    $('.live-search-list li').each(function(){
+      $(this).attr('data-search-term', $(this).text().toLowerCase());
+    });
+  });
+
+  $('.searchForItem').on('focus', function(){
+
+        $('.live-search-list').show();
+
+    });
+  $('.searchForItem').on('focusout', function(){
+
+    $('.live-search-list').hide();
+
+  });
+
+
+
+
+  $('.searchForItem').on('keyup', function(){
+
+    var searchTerm = $(this).val().toLowerCase();
+
+    $('.live-search-list li').each(function(){
+
+      if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+
+    });
+
+  });
+
+});
